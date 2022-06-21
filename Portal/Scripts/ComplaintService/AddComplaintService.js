e45966eaf1117b30e5c7f19a82de4d8b7325343f
aronamritansh <amritanshsingh.20@gmail.com>
2022-06-22 01:15:07 +0530

    bUG

--- Portal/Scripts/ComplaintService/AddComplaintService.js
+++ Portal/Scripts/ComplaintService/AddComplaintService.js
@@ -176,12 +176,18 @@
             $("#btnSave").hide();
             $("#btnUpdate").show();
             $("#btnReset").show();
+            ////$("#ddlStatus").find('option:contains("Approved")').hide();
+            ////$("#ddlStatus").find('option:contains("DisApproved")').hide();
+            ////$("#ddlStatus").find('option:contains("Closed")').hide();
         }
     }
     else {
         $("#btnSave").show();
         $("#btnUpdate").hide();
         $("#btnReset").show();
+        $("#ddlStatus").find('option:contains("Approved")').hide();
+        $("#ddlStatus").find('option:contains("DisApproved")').hide();
+        $("#ddlStatus").find('option:contains("Closed")').hide();
     }
     var complaintProductList = [];
     GetComplaintServiceProductList(complaintProductList);
@@ -908,8 +914,13 @@ function GetComplaintServiceDetail(ComplaintId) {
                 $("#txtModifiedDate").val(data.ModifiedDate);
             }
             $("#txtRemarks1").val(data.Remarks1);
-            $("#txtRemarks2").val(data.Remarks2);
-            $("#btnAddNew").show();
+            $("#txtRemarks2").val(data.Remarks2); 
+            if (data.ComplaintStatus == "2") {
+                $("#btnAddNew").hide();
+            }
+            else {
+                $("#btnAddNew").show();
+            }
             $("#btnPrint").show();
 
         },
@@ -1164,40 +1175,3 @@ function CheckMasterPermission(RoleId, InterfaceId, ModalId) {
 }
 
 
-('.Quantity').keypress(function (event) {
-    var $this = $(this);
-    if ((event.which != 46 || $this.val().indexOf('.') != -1) &&
-        ((event.which < 48 || event.which > 57) &&
-            (event.which != 0 && event.which != 8))) {
-        event.preventDefault();
-    }
-
-    var text = $(this).val();
-    if ((event.which == 46) && (text.indexOf('.') == -1)) {
-        setTimeout(function () {
-            if ($this.val().substring($this.val().indexOf('.')).length > 3) {
-                $this.val($this.val().substring(0, $this.val().indexOf('.') + 3));
-            }
-        }, 1);
-    }
-
-    if ((text.indexOf('.') != -1) &&
-        (text.substring(text.indexOf('.')).length > 2) &&
-        (event.which != 0 && event.which != 8) &&
-        ($(this)[0].selectionStart >= text.length - 2)) {
-        event.preventDefault();
-    }
-});
-
-$('.Quantity').bind("paste", function (e) {
-    var text = e.originalEvent.clipboardData.getData('Text');
-    if ($.isNumeric(text)) {
-        if ((text.substring(text.indexOf('.')).length > 3) && (text.indexOf('.') > -1)) {
-            e.preventDefault();
-            $(this).val(text.substring(0, text.indexOf('.') + 3));
-        }
-    }
-    else {
-        e.preventDefault();
-    }
-});
